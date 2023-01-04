6ca277defe3022b597504aa487673c965842c020
Ekene Ejike <61330257+Ekene95@users.noreply.github.com>
2023-01-04 11:04:48 +0100

    Add files via upload

---
+++ NRProject.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+#Function to make us anonymous
+function MAKEANONYMOUS {
+	
+	cd /root/nipe && perl nipe.pl restart
+	ANONYMOUS
+	}
+
+
+#Connecting to the remote server should be automated.
+#Function to check our anonymity.
+function ANONYMOUS {
+	
+	if [ -z "$(geoiplookup $(curl -s ifconfig.me) | grep -i NG)" ]
+	then
+		echo " [*] You are Anonymous"
+	else 
+		echo " [!] You are not Anonymous"
+		MAKEANONYMOUS
+	fi
+	
+	}
+ANONYMOUS
+# Automations on the remote server, should include:
+# Scanning targets
+# Getting information (Whois)
+
+sshpass -p ':MjsbBaU30OtuZEh' ssh -o StrictHostKeyChecking=no root@156.67.208.189 'nmap -sV -sS -sC 8.8.8.8 | grep -i open > nmapscan.txt' 
+sshpass -p ':MjsbBaU30OtuZEh' ssh -o StrictHostKeyChecking=no root@156.67.208.189 'whois 192.169.0.1 | grep -i country > whois.txt'
+
+# All collected data should be stored on the local host.
+
